38ccf4a353719c669cd5f6fd3334e4e4ac2d5806
Robert Flanagan <robertflanagan95@gmail.com>
2019-03-02 17:02:06 -0500

    adding click handling and logic to check if an image has been clicked

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,37 +1,89 @@
 import React, { Component } from "react";
+const images = [
+    {
+        "id" : 1,
+        "src" : require("../Images/1.jpg"),
+        "alt" : "Planet 1",
+        "clicked": false 
+    },
+    {
+        "id" : 2,
+        "src" : require("../Images/2.jpg"),
+        "alt" : "Planet 1",
+        "clicked": false 
+    },
+    {
+        "id" : 3,
+        "src" : require("../Images/3.jpg"),
+        "alt" : "Planet 1",
+        "clicked": false 
+    },
+    {
+        "id" : 4,
+        "src" : require("../Images/4.png"),
+        "alt" : "Planet 1",
+        "clicked": false 
+    },
+    {
+        "id" : 5,
+        "src" : require("../Images/5.jpg"),
+        "alt" : "Planet 1",
+        "clicked": false 
+    },
+    {
+        "id" : 6,
+        "src" : require("../Images/6.gif"),
+        "alt" : "Planet 1",
+        "clicked": false 
+    },
+    {
+        "id" : 7,
+        "src" : require("../Images/7.jpg"),
+        "alt" : "Planet 1",
+        "clicked": false 
+    },
+    {
+        "id" : 8,
+        "src" : require("../Images/8.png"),
+        "alt" : "Planet 1",
+        "clicked": false 
+    },
+    {
+        "id" : 9,
+        "src" : require("../Images/9.svg"),
+        "alt" : "Planet 1",
+        "clicked": false 
+    },
+
+]
 // import "./style.css";
 
 class Game extends Component {
   // Setting the component's initial state
-//   state = {
-//   };
 
-    // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-    // alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
-    // this.setState({
-    //   firstName: "",
-    //   lastName: "",
-    //   password: ""
-    // });
-//   };
+  state = {
+      images,
+      score: 0,
+      topScore: 0,
+      clickedImages: []
+  };
 
-    handleClick = event => {
-        event.preventDefault();
-        console.log("clicked");
+    handleClick = imgID => {
+        if (this.state.clickedImages.indexOf(imgID) !== -1) {
+            this.setState({ score: 0, clickedImages: [] });
+        } else {
+            this.setState({ score: this.state.score + 1, clickedImages: this.state.clickedImages.concat(imgID)});
+        }
     }
 
     render() {
         return (
             <div>
-                <img onClick={this.handleClick} alt={"Planet 1"} style={{width:50,height: 50}} src={require("../Images/1.jpg")}/>
-                <img onClick={this.handleClick} alt={"Planet 2"} style={{width:50,height: 50}} src={require("../Images/2.jpg")}/>
-                <img onClick={this.handleClick} alt={"Planet 3"} style={{width:50,height: 50}} src={require("../Images/3.jpg")}/>
-                <img onClick={this.handleClick} alt={"Planet 4"} style={{width:50,height: 50}} src={require("../Images/4.png")}/>
-                <img onClick={this.handleClick} alt={"Planet 5"} style={{width:50,height: 50}} src={require("../Images/5.jpg")}/>
-                <img onClick={this.handleClick} alt={"Planet 6"} style={{width:50,height: 50}} src={require("../Images/6.gif")}/>
-                <img onClick={this.handleClick} alt={"Planet 7"} style={{width:50,height: 50}} src={require("../Images/7.jpg")}/>
-                <img onClick={this.handleClick} alt={"Planet 8"} style={{width:50,height: 50}} src={require("../Images/8.png")}/>
-                <img onClick={this.handleClick} alt={"Planet 9"} style={{width:50,height: 50}} src={require("../Images/9.svg")}/>
+                <p>Current score: {this.state.score}</p>
+                <p>Top score: {this.state.topScore}</p>
+                {this.state.images.map(item =>
+                    <img onClick={() => this.handleClick(item.id)} key={item.id} id={item.id} alt={item.alt} style={{width:100,height:100}} src={item.src}/>
+                )}
             </div>
         );
     }
